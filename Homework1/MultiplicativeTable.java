ec328f5faaf82f8a637ccf1805faa93a06168d88
Max <maximnicola7@gmail.com>
2021-11-25 13:09:40 +0200

    Added MultiplicativeTable.java

---
+++ Homework1/MultiplicativeTable.java
@@ -0,0 +1,15 @@
+
+    public class MultiplicativeTable{
+        public static void main(String[] args){
+
+            for (int i=0; i<10; i++)
+            {
+                int n = 2;
+
+                int s = n*i; 
+
+                System.out.println(s);
+                
+            }
+        }
+    }
