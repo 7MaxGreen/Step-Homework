71978eb956529bb16b8d355069278b49d449ffe6
Max <maximnicola7@gmail.com>
2021-11-25 17:26:52 +0200

    'while' loop modified to a 'for' loop IteratingInts.java file

--- Homework1/IteratingInts.java
+++ Homework1/IteratingInts.java
@@ -2,20 +2,19 @@ public class IteratingInts {
 	
 	public static void main(String[] args) {
 		
-		int num = 200; int j;
+	int num = 3; 
 		
 		for(int i = 1; i <= num; i++) {
 			
-			j = 0;
-			
-			while(j < i) {
-				
-				System.out.print(i);
-				
-				j++;
-			}
-			
-			System.out.println();
+           // j = 0;
+            for(int j=0; i>j; j++)
+            {
+
+                System.out.print(i);
+
+            }
+
+	System.out.println();
 		}
 	}
 }
