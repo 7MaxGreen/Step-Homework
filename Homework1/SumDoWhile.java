380f3ce0a21ec692dca9693bd78409a918a29aaa
Max <maximnicola7@gmail.com>
2021-11-25 12:57:55 +0200

    SumWhile.java and SumDoWhile.java added

---
+++ Homework1/SumDoWhile.java
@@ -0,0 +1,11 @@
+public class SumDoWhile{
+    public static void main (String[] args){
+
+        int n = 2;
+
+        do{
+            n+=n;
+            System.out.println(n);
+        }while(n<=7);
+    }
+}
---
+++ Homework1/SumWhile.java
@@ -0,0 +1,13 @@
+
+public class SumWhile{
+    public static void main (String[] args){
+
+        int n = 2;
+
+        while(n<=7)
+        {
+            n+=n;
+            System.out.println(n);
+        }
+    }
+}
