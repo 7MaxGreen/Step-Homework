7e5043b54ec0d023147848ccf877d125c0fb1a96
Max <maximnicola7@gmail.com>
2021-11-28 15:13:52 +0200

    Comparator folder with App.java and NumbersComparator.java added

---
+++ Homework2_OOP/Comparator/App.java
@@ -0,0 +1,10 @@
+public class App{
+
+    public static void main(String[] args){
+
+        NumbersComparator digit = new NumbersComparator();
+
+        digit.Max(7,9.89);
+        digit.Min(7.77888888,9);
+    }
+}
---
+++ Homework2_OOP/Comparator/NumbersComparator.java
@@ -0,0 +1,18 @@
+
+public class NumbersComparator{
+    
+    public void Max(double a, double b){
+
+        if(a>b) { System.out.println("Max number is: " + a);}
+    
+        else { System.out.println("Max number is: " + b);}
+    }
+    
+
+    public void Min(double a, double b){
+
+    if(a<b) { System.out.println("Min number is: " + a);}
+
+    else {System.out.println("Min number is: " + b);}
+    }
+}
