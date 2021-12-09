c2784705fe91f3680c176de73b925cb43c324e30
Max <maximnicola7@gmail.com>
2021-12-09 18:56:20 +0200

    Changed the method sum() from int to void

--- WorkingWithArrays/src/com/step/ArraySum/ArraySum.java
+++ WorkingWithArrays/src/com/step/ArraySum/ArraySum.java
@@ -1,15 +1,13 @@
 package com.step.ArraySum;
-import java.util.Scanner;
 
 public class ArraySum {
 
-    public int sum(int[] array) {
+    public void sum(int[] array) {
         int sum = 0;
 
-        System.out.print("Sum of the array's elements is : ");
         for(int i = 0; i<array.length; i++){
             sum += array[i];
         }
-        return sum;
+        System.out.println("Sum of the array's elements is : " + sum);
     }
 }
