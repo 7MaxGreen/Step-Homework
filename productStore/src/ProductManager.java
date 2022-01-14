67178eed8c89ef89141792fa541d93c5d28ac03a
Max <maximnicola7@gmail.com>
2022-01-14 14:52:26 +0200

    create(), display() and sum() functions syntax was revised, small code refactor implemented

--- productStore/src/ProductManager.java
+++ productStore/src/ProductManager.java
@@ -1,46 +1,38 @@
 public class ProductManager {
 
     private Product[] property = new Product[6];
-    private ConsoleMenu menu = new ConsoleMenu();
-    private int counter=0;
 
     public void create(){
-    for(int i=0 ; i<property.length; i++){
-         property[counter] = new Product("Loft", 230800.8, "Dansicons");
-         property[counter] = new Product("Studio", 180800.5, "Oasis");
-         property[counter] = new Product("Villa", 330677.7, "DreamHouse Residence");
-         property[counter] = new Product("Duplex", 130677.7, "DreamHouse Residence");
-         property[counter] = new Product("Triplex", 196899.7, "SkyHouse");
-         counter ++;
-       }
+         property[1] = new Product("Loft", 230800.8, "Dansicons");
+         property[2] = new Product("Studio", 180800.5, "Oasis");
+         property[3] = new Product("Villa", 330677.7, "DreamHouse Residence");
+         property[4] = new Product("Duplex", 130677.7, "DreamHouse Residence");
+         property[5] = new Product("Triplex", 196899.7, "SkyHouse");
     }
 
+    public void display() {
+        System.out.println("Choose the property number you want to display");
 
-    public void display(){
-        System.out.println("Choose an option between 1-5 in order to display the property");
-        byte b = MyScanner.input.nextByte();
+        while (true) {
+                int userInput = MyScanner.input.nextInt();
 
-        do{
-            for(int i=0 ; i<counter ; i++){
-                if(b==i){
-                    System.out.println("Property name" + property[i].getProductName());
-                    System.out.println("Property price" + property[i].getProductPrice());
-                    System.out.println("Property manufact" + property[i].getManufacturer());
+                if (userInput > 0 && userInput < 6) {
+                    System.out.println("Property name: " + property[userInput].getProductName());
+                    System.out.println("Property price: " + property[userInput].getProductPrice());
+                    System.out.println("Property manufacturer: " + property[userInput].getManufacturer());
+                    System.out.println();
+                    break;
                 } else {
-                    System.out.println("Please choose a valid option");
+                    System.out.println("Invalid option, choose a number between 1-5");
                 }
-            }
-        }while(MyScanner.input.nextByte() != 1 || MyScanner.input.nextByte() != 2 || MyScanner.input.nextByte() != 3 || MyScanner.input.nextByte() != 4 || MyScanner.input.nextByte() != 5);
-        menu.adminMenu();
+        }
     }
 
     public void sum(){
         double sum=0;
-        for (int i=0; i<counter; i++){
+        for (int i=1; i<Product.counter; i++){
              sum = sum + property[i].getProductPrice();
-            System.out.println("sum of all properties = " + sum);
         }
-        menu.adminMenu();
+        System.out.println("Sum of all properties = " + sum);
     }
-
 }
