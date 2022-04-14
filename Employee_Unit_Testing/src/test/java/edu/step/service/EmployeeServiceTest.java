264c3ed279e3bc9d65265ec02d42cd2d6a93246d
Max <maximnicola7@gmail.com>
2022-04-14 10:58:26 +0300

    Code refactoring. @Before all working as expected

--- Employee_Unit_Testing/src/test/java/edu/step/service/EmployeeServiceTest.java
+++ Employee_Unit_Testing/src/test/java/edu/step/service/EmployeeServiceTest.java
@@ -4,51 +4,40 @@ import edu.step.model.Gender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class EmployeeServiceTest {
 
     EmployeeService service = new EmployeeService();
-    List<Employee> employeeTest = new ArrayList<>();
 
-//    @BeforeAll
-//    public void ListInit(){
-//        employeeTest.add(new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343));
-//        employeeTest.add(new Employee(1, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343));
-//        employeeTest.add(new Employee(2, "Camila", LocalDate.of(1988, 5, 22), Gender.FEMALE, 2189.2));
-//        employeeTest.add(new Employee(3, "Elsa", LocalDate.of(1993, 3, 25), Gender.FEMALE, 1822.31));
-//    }
+    static List<Employee> employeeTest = new ArrayList<>();
 
-    @Test
-    public void findMales_notNullMales(){
 
-        Employee e1  = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-
+    @BeforeAll
+    public static void ListInit(){
+        employeeTest.add(new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343));
         employeeTest.add(new Employee(1, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343));
         employeeTest.add(new Employee(2, "Camila", LocalDate.of(1988, 5, 22), Gender.FEMALE, 2189.2));
         employeeTest.add(new Employee(3, "Elsa", LocalDate.of(1993, 3, 25), Gender.FEMALE, 1822.31));
 
-        List<String> newList = service.findMales(employeeTest);
+    }
+
+    @Test
+    public void findMales_notNullMales(){
+
+        Employee e1  = new Employee(1, "Ben", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
+        employeeTest.add(e1);
+
+       List<String> newList = service.findMales(employeeTest);
 
         Assertions.assertNotNull(newList);
     }
 
     @Test
     public void findMales_listSizeMales(){
-
-        Employee e1  = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-
-        employeeTest.add(new Employee(1, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343));
-        employeeTest.add(new Employee(2, "Camila", LocalDate.of(1988, 5, 22), Gender.FEMALE, 2189.2));
-        employeeTest.add(new Employee(3, "Elsa", LocalDate.of(1993, 3, 25), Gender.FEMALE, 1822.31));
-
         List<String> newList = service.findMales(employeeTest);
 
         Assertions.assertEquals(2, newList.size());
@@ -58,15 +47,10 @@ public class EmployeeServiceTest {
     @Test
     public void findNames_expectedName(){
 
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(1, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e3 = new Employee(2, "Camila", LocalDate.of(1988, 5, 22), Gender.FEMALE, 2189.2);
-        Employee e4 = new Employee(3, "Elsa", LocalDate.of(1993, 3, 25), Gender.FEMALE, 1822.31);
-
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-        employeeTest.add(e3);
-        employeeTest.add(e4);
+        Employee e1 = employeeTest.get(0);
+        Employee e2 = employeeTest.get(1);
+        Employee e3 = employeeTest.get(2);
+        Employee e4 = employeeTest.get(3);
 
         List<String> newList = service.findNames(employeeTest);
 
@@ -79,47 +63,23 @@ public class EmployeeServiceTest {
 
     @Test
     public void findNames_expectedSize(){
-
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e3 = new Employee(3, "Camila", LocalDate.of(1988, 5, 22), Gender.FEMALE, 2189.2);
-        Employee e4 = new Employee(4, "Elsa", LocalDate.of(1993, 3, 25), Gender.FEMALE, 1822.31);
-
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-        employeeTest.add(e3);
-        employeeTest.add(e4);
-
         Assertions.assertEquals(4, employeeTest.size());
 
     }
 
     @Test
     public void findNames_containsObjectName(){
-
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e3 = new Employee(3, "Camila", LocalDate.of(1988, 5, 22), Gender.FEMALE, 2189.2);
-        Employee e4 = new Employee(4, "Elsa", LocalDate.of(1993, 3, 25), Gender.FEMALE, 1822.31);
-
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-        employeeTest.add(e3);
-        employeeTest.add(e4);
-
         List<String> newList = service.findNames(employeeTest);
 
         Assertions.assertTrue(newList.contains(employeeTest.get(0).getName()));
+        Assertions.assertTrue(newList.contains(employeeTest.get(1).getName()));
+        Assertions.assertTrue(newList.contains(employeeTest.get(2).getName()));
+        Assertions.assertTrue(newList.contains(employeeTest.get(3).getName()));
 
     }
 
     @Test
     public void findUniqueBirthdate_expectedObjectDate (){
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-
         List<LocalDate> newList = service.findUniqueBirthdate(employeeTest);
 
         Assertions.assertEquals(newList.get(0), employeeTest.get(0).getBirthdate());
@@ -128,12 +88,7 @@ public class EmployeeServiceTest {
 
     @Test
     public void findUniqueBirthdate_expectedDate (){
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-
-        List<LocalDate> newList = service.findUniqueBirthdate(employeeTest);
+      List<LocalDate> newList = service.findUniqueBirthdate(employeeTest);
 
         Assertions.assertEquals(LocalDate.of(1961, 5, 17), newList.get(0));
 
@@ -141,11 +96,6 @@ public class EmployeeServiceTest {
 
     @Test
     public void findRequestedName_expectedName(){
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-
         List<Employee> newList =  service.findRequestedName(employeeTest, "Adam");
 
         Assertions.assertEquals("Adam", newList.get(0).getName());
@@ -153,23 +103,13 @@ public class EmployeeServiceTest {
 
     @Test
     public void calculateAverageSalary_expectedSalary(){
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-        employeeTest.add(e2);
+        Double newSalary = service.calculateAverageSalary(employeeTest);
 
-        Double newList = service.calculateAverageSalary(employeeTest);
-
-        Assertions.assertEquals(2300.4343, newList);
+        Assertions.assertEquals(2153.09465, newSalary);
     }
 
     @Test
     public void calculateAverageSalary_notExpectedSalary(){
-        Employee e1 = new Employee(1, "Joe", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        Employee e2 = new Employee(2, "Adam", LocalDate.of(1961, 5, 17), Gender.MALE, 2300.4343);
-        employeeTest.add(e1);
-        employeeTest.add(e2);
-
         Double newList = service.calculateAverageSalary(employeeTest);
 
         Assertions.assertNotEquals(23, (double) newList, 0.0);
